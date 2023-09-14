2c1afd987c413025eef3ed4d65a196df053f8624
ClaytonMalarkey <malarkey1985@gmail.com>
2023-09-13 20:13:59 -0600

    third commit

--- js/app.js
+++ js/app.js
@@ -1,2 +1,8 @@
 var names = ["clate", "cate", "kate", "cait", "kait"];
 var ages = [38, 37, 35, 43, 44];
+var firstUserName = names[1];
+var firstUserAge = ages[1];
+var lastUserName = names[4];
+var lastUserAge = ages[4];
+var middleUserName = names[2];
+var middleUserAge = ages[2];
